ecc7c8bfa0e26d895ddf0c59898a824162a40c2d
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-05-03 15:53:13 -0600

    edit and add functionality

--- server/controller.js
+++ server/controller.js
@@ -17,9 +17,36 @@ module.exports = {
         res.status(200).send(houses)
     },
     createHouse: (req, res) => {
+        let {address, price, imageURL} = req.body;
 
+        let newHouse = {
+            id: globalId,
+            address,
+            price,
+            imageURL
+        }
+
+        // console.log(newHouse)
+        houses.push(newHouse);
+        res.status(200).send(houses);
+        globalId++;
     },
     updateHouse: (req, res) => {
+        let {id} = req.params;
+        let {type} = req.body;
 
+        let index = houses.findIndex((house) => {
+            return house.id === +id;
+        })
+
+        if ((houses[index].price <= 10000) && type === `minus`) {
+            res.status(400).send(`cannot go below $0`);
+        } else if (type === `plus`){
+            houses[index].price += 10000;
+            res.status(200).send(houses)
+        } else if (type === `minus`) {
+            houses[index].price -= 10000;
+            res.status(200).send(houses);
+        }
     }
 }
