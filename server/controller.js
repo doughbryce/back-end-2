ed5181108e80d5a717709036faa67cac1bf83ffe
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-05-03 15:10:27 -0600

    delete functional

--- server/controller.js
+++ server/controller.js
@@ -3,15 +3,23 @@ let globalId = 4;
 
 module.exports = {
     getHouses: (req, res) => {
-
+        res.status(200).send(houses);
+        // console.log(req.params)
     },
     deleteHouse: (req, res) => {
 
+        // we need to use findIndex because the index != the id
+        let index = houses.findIndex((house) => {
+            return house.id === +req.params.id;
+        })
+
+        houses.splice(index, 1);
+        res.status(200).send(houses)
     },
     createHouse: (req, res) => {
 
     },
     updateHouse: (req, res) => {
-        
+
     }
 }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const {getHouses, createHouse, updateHouse, deleteHouse} = require(`./controller.js`);
+
+app.get(`/api/houses`, getHouses);
+app.post(`/api/houses`, createHouse);
+app.put(`/api/houses/:id`, updateHouse);
+app.delete(`/api/houses/:id`, deleteHouse);
 
 
 let SERVER_PORT = 4004;
